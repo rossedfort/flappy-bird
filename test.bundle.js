96c9cf4055e4917d906080c584420497953b6141
rossedfort <rossedfort@gmail.com>
2016-02-18 10:40:11 -0700

    done

--- test.bundle.js
+++ test.bundle.js
@@ -10639,8 +10639,8 @@
 	// Hot Module Replacement
 	if(false) {
 		// When the styles change, update the <style> tags
-		module.hot.accept("!!/Users/robbielane/turing/mod_4/game_time/node_modules/mocha-loader/node_modules/css-loader/index.js!/Users/robbielane/turing/mod_4/game_time/node_modules/mocha/mocha.css", function() {
-			var newContent = require("!!/Users/robbielane/turing/mod_4/game_time/node_modules/mocha-loader/node_modules/css-loader/index.js!/Users/robbielane/turing/mod_4/game_time/node_modules/mocha/mocha.css");
+		module.hot.accept("!!/Users/rossedfort/code/mod_4/projects/personal-flappy-bird/node_modules/mocha-loader/node_modules/css-loader/index.js!/Users/rossedfort/code/mod_4/projects/personal-flappy-bird/node_modules/mocha/mocha.css", function() {
+			var newContent = require("!!/Users/rossedfort/code/mod_4/projects/personal-flappy-bird/node_modules/mocha-loader/node_modules/css-loader/index.js!/Users/rossedfort/code/mod_4/projects/personal-flappy-bird/node_modules/mocha/mocha.css");
 			if(typeof newContent === 'string') newContent = [[module.id, newContent, '']];
 			update(newContent);
 		});
